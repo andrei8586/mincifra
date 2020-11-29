162bf512f5eac8e5c1ea63e83b2c38ec65cad7e8
andrei8586 <34164779+andrei8586@users.noreply.github.com>
2020-11-29 10:46:25 +0300

    Add files via upload

--- assets/js/regions.js
+++ assets/js/regions.js
@@ -1,201 +1,197 @@
-var REGIONS_DATA = {
-    region: {
-        title: 'Регион',
-        items: [{
-            id: '001',
-            title: 'Страны мира'
-        }, {
-            id: 'BY',
-            title: 'Беларусь'
-        }, {
-            id: 'KZ',
-            title: 'Казахстан'
-        }, {
-            id: 'RU',
-            title: 'Россия'
-        }, {
-            id: 'TR',
-            title: 'Турция'
-        }, {
-            id: 'UA',
-            title: 'Украина'
-        }]
-    },
-    lang: {
-        title: 'Язык',
-        items: [{
-            id: 'en',
-            title: 'Английский'
-        }, {
-            id: 'be',
-            title: 'Белорусский'
-        }, {
-            id: 'kk',
-            title: 'Казахский'
-        }, {
-            id: 'ru',
-            title: 'Русский'
-        }, {
-            id: 'tr',
-            title: 'Турецкий'
-        }, {
-            id: 'uk',
-            title: 'Украинский'
-        }]
-    },
-    quality: {
-        title: 'Точность границ',
-        items: [{
-            id: '0',
-            title: '0'
-        }, {
-            id: '1',
-            title: '1'
-        }, {
-            id: '2',
-            title: '2'
-        }, {
-            id: '3',
-            title: '3'
-        }]
-    }
-},
-// Шаблон html-содержимого макета.
-optionsTemplate = [
-    '<div style="line-height: 34px; background-color: #80808080;" id="regions-params">',
-    '{% for paramName, param in data.params %}',
-    '{% for key, value in state.values %}',
-    '{% if key == paramName %}',
-    '<div class="btn-group btn-group-xs">',
-    '<button{% if state.enabled %}{% else %} disabled{% endif %} type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">',
-    '<span>{{ param.title }}</span>',
-    '<span class="value">: {{ value }}</span>',
-    '&nbsp;<span class="caret"></span>',
-    '</button>',
-    '<ul class="dropdown-menu {{ paramName }}">',
-    '{% for item in param.items %}',
-    '<li{% if item.id == value %} class="active"{% endif %}>',
-    '<a href="#" data-param="{{ paramName }}" data-id="{{ item.id }}">',
-    '{{ item.title }}',
-    '</a>',
-    '</li>',
-    '{% endfor %}',
-    '</ul>',
-    '</div>&nbsp;',
-    '{% endif %}',
-    '{% endfor %}',
-    '{% endfor %}',
-    '</div>'
-].join('');
 
-ymaps.ready(init);
 
-function init() {
-// Создадим собственный макет RegionControl.
-var RegionControlLayout = ymaps.templateLayoutFactory.createClass(optionsTemplate, {
-        build: function () {
-            RegionControlLayout.superclass.build.call(this);
-            this.handleClick = ymaps.util.bind(this.handleClick, this);
-            $(this.getParentElement)
-                .on('click', 'a', this.handleClick);
-        },
-        clear: function () {
-            $(this.getParentElement)
-                .off('click', 'a', this.handleClick);
-            RegionControlLayout.superclass.clear.call(this);
-        },
-        handleClick: function (e) {
-            e.preventDefault();
-            var $target = $(e.currentTarget);
-            var state = this.getData().state;
-            var newValues = ymaps.util.extend({}, state.get('values'));
-            if (!$target.hasClass('active')) {
-                newValues[$target.data('param')] = $target.data('id');
-                state.set('values', newValues);
-            }
-        }
-    }),
-    // Наследуем класс нашего контрола от ymaps.control.Button.
-    RegionControl = ymaps.util.defineClass(function (parameters) {
-        RegionControl.superclass.constructor.call(this, parameters);
-        this.regions = new ymaps.GeoObjectCollection();
-    }, ymaps.control.Button, /** @lends ymaps.control.Button */{
-        onAddToMap: function (map) {
-            RegionControl.superclass.onAddToMap.call(this, map);
-            map.geoObjects.add(this.regions);
-            this.setupStateMonitor();
-            this.loadRegions(this.state.get('values'));
-        },
+$('path').hover(function(e){
 
-        onRemoveFromMap: function (map) {
-            map.geoObjects.remove(this.regions);
-            this.clearStateMonitor();
-            RegionControl.superclass.onRemoveFromMap.call(this, map);
-        },
+  $('path').css('fill','#fff');
+  $('.indicator').html('');
+  var id = $(this).attr('id').toUpperCase();
 
-        setupStateMonitor: function () {
-            this.stateMonitor = new ymaps.Monitor(this.state);
-            this.stateMonitor.add('values', this.handleStateChange, this);
-        },
+  if($(this).attr('name')) {
+    var name = $(this).attr('name');
+    $('<div>' + name +'</div>').appendTo('.indicator');
+  }
 
-        clearStateMonitor: function () {
-            this.stateMonitor.removeAll();
-        },
 
-        handleStateChange: function (params) {
-            this.loadRegions(params);
-        },
+  if($(this).attr('flag')) {
+    var flag = "http://sandbox.alexti-dev.ru/mapping/" + $(this).attr('flag') ;
+    $(' <img class="flag" src="" alt="">').appendTo('.indicator');
+    $('.indicator').find('img').attr('src',flag )
+   // $('<img src='+ flag+ ' >').appendTo('.indicator');
+ }
 
-        handleRegionsLoaded: function (res) {
-            this.regions
-                .removeAll()
-                .add(res.geoObjects);
-            this.getMap().setBounds(
-                this.regions.getBounds(),
-                {checkZoomRange: true}
-            );
-        },
+ $('.change').remove();
 
-        loadRegions: function (params) {
-            this.disable();
-            return ymaps.regions.load(params.region, params)
-                .then(this.handleRegionsLoaded, this)
-                .always(this.enable, this);
-        }
-    }),
+ var script = document.createElement('script');
+ // script.classList.add('change');
+  //script.src = 'http://api.geonames.org/countryInfoJSON?country='+info[id]+'&username=pixeltest&style=full&callback=update';
+  document.body.appendChild(script);
 
-    map = new ymaps.Map('map', {
-        center: [50, 30],
-        zoom: 3,
-        controls: ['typeSelector']
-    }, {
-        typeSelectorSize: 'small'
-    }),
+  $(this).css('fill','#f6e72d');
+  $('path').not(this).css('fill','rgba(0,0,0,0.5)');
+  $('.indicator').css({'top':e.pageY,'left':e.pageX+30}).show();
 
-    // Создадим экземпляр RegionControl.
-    regionControl = new RegionControl({
-        state: {
-            enabled: true,
-            values: {
-                region: 'RU',
-                lang: 'ru',
-                quality: '1'
-            }
-        },
-        data: {
-            params: REGIONS_DATA
-        },
-        options: {
-            layout: RegionControlLayout
-        },
-        float: 'left',
-        maxWidth: [300]
-    });
 
-// Добавим контрол на карту.
-map.controls.add(regionControl);
-// Узнавать о изменениях параметров RegionControl можно следующим образом.
-regionControl.events.add('statechange', function (e) {
-    console.log(e.get('target').get('values'));
+
+},function(){
+  $('.indicator').html('');
+  $('.indicator').hide();
+  $('path').css('fill','rgba(0,0,0,0.2)');
 });
+
+
+
+
+
+var idAarr = ["RU-MOW", "RU-SPE", "RU-NEN", "RU-YAR", "RU-CHE", "RU-ULY", "RU-TYU", "RU-TUL", "RU-SVE", "RU-RYA", "RU-ORL", "RU-OMS", "RU-NGR", "RU-LIP", "RU-KRS", "RU-KGN", "RU-KGD", "RU-IVA", "RU-BRY", "RU-AST", "RU-KHA", "RU-CE", "RU-UD", "RU-SE", "RU-MO", "RU-KR", "RU-KL", "RU-IN", "RU-AL", "RU-BA", "RU-AD", "RU-CR", "RU-SEV", "RU-KO", "RU-KIR", "RU-PNZ", "RU-TAM", "RU-MUR", "RU-LEN", "RU-VLG", "RU-KOS", "RU-PSK", "RU-ARK", "RU-YAN", "RU-CHU", "RU-YEV", "RU-TY", "RU-SAK", "RU-AMU", "RU-BU", "RU-KK", "RU-KEM", "RU-NVS", "RU-ALT", "RU-DA", "RU-STA", "RU-KB", "RU-KC", "RU-KDA", "RU-ROS", "RU-SAM", "RU-TA", "RU-ME", "RU-CU", "RU-NIZ", "RU-VLA", "RU-MOS", "RU-KLU", "RU-BEL", "RU-ZAB", "RU-PRI", "RU-KAM", "RU-MAG", "RU-SA", "RU-KYA", "RU-ORE", "RU-SAR", "RU-VGG", "RU-VOR", "RU-SMO", "RU-TVE", "RU-PER", "RU-KHM", "RU-TOM", "RU-IRK"];
+var idAarr2 = new Array(
+  ["RU-MOW",  "Москва", "moscow.gif"],
+  ["RU-CHE", "Челябинская область", "chelyabinskaya_flag.png" ],
+  ["RU-ORL",  "Орловская область"],
+  ["RU-OMS",  "Омская область", "flag_omskoj_oblasti.png"],
+  ["RU-LIP",  "Липецкая область", "lipeckya.jpg"],
+  ["RU-KRS",  "Курская область", "flag_of_kursk_oblast.png"],
+  ["RU-RYA",  "Рязанская область", "ryazan.png"],
+  ["RU-BRY",  "Брянская область", "bryanskaya_flag.png"],
+  ["RU-KIR",  "Кировская область", "flag_kirovskoy_oblasti.png"],
+  ["RU-ARK",  "Архангельская область", ""],
+  ["RU-MUR",  "Мурманская область", ""],
+  ["RU-SPE",  "Санкт-Петербург", ""],
+  ["RU-YAR",  "Ярославская область", ""],
+  ["RU-ULY",  "Ульяновская область", ""],
+  ["RU-NVS",  "Новосибирская область", ""],
+  ["RU-TYU",  "Тюменская область", ""],
+  ["RU-SVE",  "Свердловская область", ""],
+  ["RU-NGR",  "Новгородская область", ""],
+  ["RU-KGN",  "Курганская область", ""],
+  ["RU-KGD",  "Калининградская область", ""],
+  ["RU-IVA",  "Ивановская область", ""],
+  ["RU-AST",  "Астраханская область", ""],
+  ["RU-KHA",  "Хабаровский край", ""],
+  ["RU-CE",  "Чеченская республика", ""],
+  ["RU-UD",  "Удмуртская республика", ""],
+  ["RU-SE",  "Республика Северная Осетия", ""],
+  ["RU-MO",  "Республика Мордовия", ""],
+  ["RU-KR",  "Республика  Карелия", ""],
+  ["RU-KL",  "Республика  Калмыкия", ""],
+  ["RU-IN",  "Республика  Ингушетия", ""],
+  ["RU-AL",  "Республика Алтай", ""],
+  ["RU-BA",  "Республика Башкортостан", ""],
+  ["RU-AD",  "Республика Адыгея", ""],
+  ["RU-CR",  "Республика Крым", ""],
+  ["RU-SEV",  "Севастополь", ""],
+  ["RU-KO",  "Республика Коми", ""],
+  ["RU-PNZ",  "Пензенская область", ""],
+  ["RU-TAM",  "Тамбовская область", ""],
+  ["RU-LEN",  "Ленинградская область", ""],
+  ["RU-VLG",  "Вологодская область", ""],
+  ["RU-KOS",  "Костромская область", ""],
+  ["RU-PSK",  "Псковская область", ""],
+  ["RU-YAN",  "Ямало-Ненецкий АО", ""],
+  ["RU-CHU",  "Чукотский АО", ""],
+  ["RU-YEV",  "Еврейская автономская область", ""],
+  ["RU-TY",  "Республика Тыва", ""],
+  ["RU-SAK",  "Сахалинская область", ""],
+  ["RU-AMU",  "Амурская область", ""],
+  ["RU-BU",  "Республика Бурятия", ""],
+  ["RU-KK",  "Республика Хакасия", ""],
+  ["RU-KEM",  "Кемеровская область", ""],
+  ["RU-ALT",  "Алтайский край", ""],
+  ["RU-DA",  "Республика Дагестан", ""],
+  ["RU-KB",  "Кабардино-Балкарская республика", ""],
+  ["RU-KC",  "Карачаевая-Черкесская республика", ""],
+  ["RU-KDA",  "Краснодарский край", ""],
+  ["RU-ROS",  "Ростовская область", ""],
+  ["RU-SAM",  "Самарская область", ""],
+  ["RU-TA",  "Республика Татарстан", ""],
+  ["RU-ME",  "Республика Марий Эл", ""],
+  ["RU-CU",  "Чувашская республика", ""],
+  ["RU-NIZ",  "Нижегородская край", ""],
+  ["RU-VLA",  "Владимировская область", ""],
+  ["RU-MOS",  "Московская область", ""],
+  ["RU-KLU",  "Калужская область", ""],
+  ["RU-BEL",  "Белгородская область", ""],
+  ["RU-ZAB",  "Забайкальский край", ""],
+  ["RU-PRI",  "Приморский край", ""],
+  ["RU-KAM",  "Камачатский край", ""],
+  ["RU-MAG",  "Магаданская область", ""],
+  ["RU-SA",  "Республика Саха", ""],
+  ["RU-KYA",  "Красноярский край", ""],
+  ["RU-ORE",  "Оренбургская область", ""],
+  ["RU-SAR",  "Саратовская область", ""],
+  ["RU-VGG",  "Волгоградская область", ""],
+  ["RU-VOR",  "Ставропольский край", ""],
+  ["RU-SMO",  "Смоленская область", ""],
+  ["RU-TVE",  "Тверская область", ""],
+  ["RU-PER",  "Пермская область", ""],
+  ["RU-KHM",  "Ханты-Мансийский АО", ""],
+  ["RU-KHM",  "Ханты-Мансийский АО", ""],
+  ["RU-TOM",  "Томская область", ""],
+  ["RU-IRK",  "Иркутская область", ""],
+  ["RU-NEN",  "Ненецскй АО", ""],
+  ["RU-STA",  "Ставропольский край", ""],
+  ["RU-TUL",  "Тульская область", "tulskaya_flag.png"]
+
+  );
+
+$('path').each(function() {
+
+  var regId = $(this).attr('id');
+  var flag = '';
+  var name = '';
+  for (var j = 0; j < idAarr2.length; j++) {
+
+    if (regId == idAarr2[j][0]) {
+      name = idAarr2[j][1];
+      flag =  'flags/'+ idAarr2[j][2];
+
+      $(this).attr('name', name);
+      $(this).attr('flag', flag);
+    }
+  }
+
+
+  var regIdDiv = '<div class="reg" >'+ '[' + '<span>'+  regId +'</span>' + ']' +' '+ name +'</div>'
+  $(regIdDiv).appendTo('.regs');
+
+
+// var idArrMin = [regId, '_'];
+// idAarr2.push(idArrMin);
+
+
+})
+
+
+// for (var j = 0; j < idAarr2.length; j++) {
+//   if (regId == idAarr2[j][0]) {
+//     name = cyr2latChars[j][1];
+
+//   }
+// }
+
+
+function naming() {
+
 }
+
+
+//revertFill();
+
+
+$('.reg').hover(function(e) {
+
+
+  var id = $(this).find('span').text();
+
+  idHover = '#' + id;
+
+  $(idHover).css('fill','#f6e72d');
+ // $('path').not(this).css('fill','rgba(0,0,0,0.5)');
+ // $('.indicator').css({'top':e.pageY,'left':e.pageX+30}).show();
+
+},function(){
+  $('.indicator').html('');
+  $('.indicator').hide();
+  $('path').css('fill','rgba(0,0,0,0.2)');
+});
+
+//} // revertFill
